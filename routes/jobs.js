1946fb3c5db658f43894f723b08e461e5e9b24d2
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-08-20 11:24:01 -0400

    add all CRUD routes for jobs, simply have routes sending a string response

---
+++ routes/jobs.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+// @route   GET api/jobs
+// @desc    Get all users jobs
+// @access  Private
+router.get('/', (req, res) => {
+  res.send('Get all users jobs');
+});
+
+// @route   POST api/jobs
+// @desc    Add new job
+// @access  Private
+router.post('/', (req, res) => {
+  res.send('Add job');
+});
+
+// @route   PUT api/jobs/:id
+// @desc    Update job
+// @access  Private
+router.put('/:id', (req, res) => {
+  res.send('Update job');
+});
+
+// @route   DELETE api/jobs/:id
+// @desc    Delete job
+// @access  Private
+router.delete('/:id', (req, res) => {
+  res.send('Delete job');
+});
+
+module.exports = router;
